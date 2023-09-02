54cc641d9f45717e6717afc22df82fc873c7e0c6
NEKOSUKKI <142430384+NEKOSUKKI@users.noreply.github.com>
2023-09-02 17:48:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+import re
+import os.path
+import urllib.request
+import csv
+
+def main(url, folder_name, csv_file_name):
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    text = response.text
+    result = re.findall('<div class="name-group">.*?</div>', text, re.DOTALL)
+    result1 = re.findall('<span class="name">.*?</span>', text, re.DOTALL)
+    result2 = re.findall('<span class="iden">.*?</span>', text, re.DOTALL)
+    result3 = re.findall('<img src="/__local/.*?jpg" alt', text, re.DOTALL)
+    names = []
+    for item in result1:
+        match = re.search('>([\u4e00-\u9fa5]+)<', item)
+        if match:
+            names.append(match.group(1))
+
+    idens = []
+    for item in result2:
+        match = re.search('>([\u4e00-\u9fa5]+)<', item)
+        if match:
+            idens.append(match.group(1))
+
+    imgs = []
+    for item in result3:
+        start_index = item.index('"') + 1
+        end_index = item.index('"', start_index)
+        img = "https://sdmda.bupt.edu.cn/" + item[start_index:end_index]
+        imgs.append(img)
+
+    folder_path = os.path.join(os.getcwd(), folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for i, item in enumerate(imgs):
+        filename = f"image{i + 1}.jpg"
+        save_path = os.path.join(folder_path, filename)
+        urllib.request.urlretrieve(item, save_path)
+
+    csv_data = zip(names, idens, imgs)
+    csv_file = os.path.join(folder_path, csv_file_name)
+    with open(csv_file, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Name', 'Department', 'Image'])
+        writer.writerows(csv_data)
+
+
+main("https://sdmda.bupt.edu.cn/szdw/js.htm", "数媒院教授信息", "professors_information.csv")
+main("https://sdmda.bupt.edu.cn/szdw/fjs.htm", "数媒副教授信息", "associate professor_information.csv")
+main("https://sdmda.bupt.edu.cn/szdw/js1.htm", "数媒院讲师信息", "lecturer_information.csv")
